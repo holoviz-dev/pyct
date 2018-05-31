b8d47d8d1b86002c3d1c76683491540a0bd9d536
Chris B <ceball@gmail.com>
2018-05-31 15:58:44 +0100

    Added license, classifiers, etc.

--- setup.py
+++ setup.py
@@ -4,13 +4,22 @@ import version
 
 setup_args = dict(
     name = 'pyct',
-    description = 'python package common tasks for users (e.g. install examples, download data, ...',
+    description = 'python package common tasks for users (e.g. copy examples, fetch data, ...)',
     version = version.get_setup_version('pyct'),
     license = 'BSD-3',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",    
     url = 'http://github.com/pyviz/pyct',
     packages = find_packages(),
     python_requires=">=2.7",
     include_package_data = True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',        
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],    
     install_requires=[
         'pyyaml',
         'requests'
