d33e186896ba0ba8fe2b4483923658ceb865fa09
Chris B <ceball@gmail.com>
2018-06-23 10:32:37 +0100

    flakes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 # Temporary until build requirements as specified in pyproject.toml
 # are widely supported
 try:
-    import pyct.build # TODO: will be removed when param supplies get_setup_version2()
-    import param
+    import pyct.build # noqa: TODO will be removed when param supplies get_setup_version2()
+    import param      # noqa: see import error message below
 except ImportError:
     raise ImportError("pyct requires requires param to build; please upgrade to pip>=10 and try again (or alternatively, install param manually first (e.g. `pip install param` or `conda install -c pyviz param`)")
 
