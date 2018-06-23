c94dd53d13b7d6ffba880bc204e832c8f5e47359
Chris B <ceball@gmail.com>
2018-06-23 16:29:30 +0100

    Clarified build dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
-
-# Temporary until build requirements as specified in pyproject.toml
-# are widely supported
+####
+# Build dependency checks
+#
+# Temporary, until pyproject.toml is widely supported. We're expecting
+# most users to install a wheel or conda package, neither of which
+# requires running setup.py and building a package.  So these checks
+# are for packagers and those installing from e.g. github.
+import setuptools
+from pkg_resources import parse_version
+missing_build_dep = False
+if parse_version(setuptools.__version__)<parse_version('30.3.0'):
+    missing_build_dep = True
 try:
     import pyct.build # noqa: TODO will be removed when param supplies get_setup_version2()
-    import param      # noqa: see import error message below
-except ImportError:
-    raise ImportError("pyct requires requires param to build; please upgrade to pip>=10 and try again (or alternatively, install param manually first (e.g. `pip install param` or `conda install -c pyviz param`)")
+    import param      # noqa: see missing dep message below
+    if parse_version(param.__version__)<parse_version('1.6.1'):
+        missing_build_dep = True
+except:
+    missing_build_dep = True
+
+if missing_build_dep:
+    raise ValueError('Building pyct requires setuptools>=30.3.0 and param>=1.6.1; please upgrade to pip>=10 and try again. Alternatively, install the build dependencies manually first (e.g. `pip install --upgrade "setuptools>=30.3.0" "param>=1.6.1"` or `conda install -c pyviz "setuptools>=30.3.0" "param>=1.6.1"`)')
+#####
 
 if __name__ == "__main__":
     setup()
