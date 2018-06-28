5dcec653ef956b366c18e464f4784d3587c71bb2
Chris B <ceball@gmail.com>
2018-06-28 11:40:34 +0100

    get_setupcfg_version moved to param

--- pyct/build.py
+++ pyct/build.py
@@ -21,38 +21,3 @@ def examples(path, root, verbose=False, force=False):
         shutil.copytree(tree_root, path, ignore=ignore, symlinks=True)
     else:
         print('Cannot find %s' % tree_root)
-
-
-## temporary; should move to autover
-
-def get_setup_version2():
-    # Simpler get_setup_version() for setup.cfg files.
-    # Requires [tool:autover] in setup.cfg
-    from param.version import Version # undeclared dependency, but this fn will move to version anyway
-    try:
-        import configparser
-    except ImportError:
-        import ConfigParser as configparser
-    import warnings
-    config = configparser.ConfigParser()
-    config.read("setup.cfg")
-    
-    reponame = config.get('tool:autover','reponame')
-    pkgname = config.get('tool:autover','pkgname',vars={'pkgname':reponame})
-
-    ###
-    # setuptools requires % to be escaped with % or it can't parse
-    # setup.cfg, but then git export-subst wouldn't work :(
-    # So we hack such things into section headings like
-    # [tool:autover.configparser_workaround.archive_commit=$Format:%h$]
-    archive_commit = None
-    archive_commit_key = 'tool:autover.configparser_workaround.archive_commit'
-    for section in config.sections():
-        if section.startswith(archive_commit_key):
-            archive_commit = re.match(".*=\s*(\S*)\s*",section).group(1)
-    if archive_commit is None:
-        warnings.warn("No archive commit available in setup.cfg; consequences = ...")
-    ###
-
-    # TODO: is this right? do I need the other stuff from projects' get_setup_version()?
-    return Version.setup_version(os.path.dirname(os.path.abspath("setup.cfg")),reponame,pkgname=pkgname,archive_commit=archive_commit)
