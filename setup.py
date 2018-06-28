000247b21f0e908507155eff57b8c756eda725ea
Chris B <ceball@gmail.com>
2018-06-28 15:14:39 +0100

    Set minimum required build and run time dependencies to param 1.7.0.
    The number of files where this is required reflects (a) the limited current support for pyproject.toml, and (b) build and run time dependencies.

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ if parse_version(setuptools.__version__)<parse_version('30.3.0'):
     missing_build_dep = True
 try:
     import param
-    if parse_version(param.__version__)<parse_version('1.6.1'):
+    if parse_version(param.__version__)<parse_version('1.7.0'):
         missing_build_dep = True
 except:
     missing_build_dep = True
 
 if missing_build_dep:
-    raise ValueError('Building pyct requires setuptools>=30.3.0 and param>=1.6.1; please upgrade to pip>=10 and try again. Alternatively, install the build dependencies manually first (e.g. `pip install --upgrade "setuptools>=30.3.0" "param>=1.6.1"` or `conda install -c pyviz "setuptools>=30.3.0" "param>=1.6.1"`)')
+    raise ValueError('Building pyct requires setuptools>=30.3.0 and param>=1.7.0; please upgrade to pip>=10 and try again. Alternatively, install the build dependencies manually first (e.g. `pip install --upgrade "setuptools>=30.3.0" "param>=1.7.0"` or `conda install -c pyviz "setuptools>=30.3.0" "param>=1.7.0"`)')
 #####
 
 if __name__ == "__main__":
