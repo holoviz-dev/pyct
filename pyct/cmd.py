daf83037a5300ef63ac210087f7ff3dee4bbea7f
Chris B <ceball@users.noreply.github.com>
2018-10-31 16:39:14 +0000

    Support --use-test-data (#63)
    * Now allows substitution of 'test data', reading from matching file in .data_stubs
    * Added cleanup command to remove the substituted files
    * Added pytest-based tests

--- pyct/cmd.py
+++ pyct/cmd.py
@@ -9,6 +9,7 @@ import importlib
 import inspect
 import argparse
 import distutils.dir_util
+import shutil
 
 def _find_examples(name):
     module_path = os.path.dirname(inspect.getfile(importlib.import_module(name)))
@@ -24,10 +25,16 @@ def _find_examples(name):
 
     raise ValueError("Could not find examples for %s at any of %s"%(name,candidates))
 
-def examples(name,path,verbose=False,force=False):
-    """Copy examples and fetch data (if any) to the supplied path. See copy-examples and fetch-data for more flexibility."""
+def examples(name,path,verbose=False,use_test_data=False,force=False):
+    """
+    Copy examples and fetch data (if any) to the supplied path.
+    See copy-examples and fetch-data for more flexibility.
+
+    NOTE: force operates both on example and data over-writing
+    pre-existing files.
+    """
     copy_examples(name, path, verbose, force)
-    fetch_data(name,path,require_datasets=False)
+    fetch_data(name,path,require_datasets=False,use_test_data=use_test_data,force=force)
 
 
 def copy_examples(name,path,verbose=False,force=False):
@@ -80,7 +87,7 @@ except ImportError:
 #        print('this download script requires the requests module: conda install requests')
 #        sys.exit(1)
 
-    
+
 STREAM = sys.stderr
 
 BAR_TEMPLATE = '%s[%s%s] %i/%i - %s\r'
@@ -96,6 +103,8 @@ ETA_INTERVAL = 1
 # How many intervals (excluding the current one) to calculate the simple moving
 # average
 ETA_SMA_WINDOW = 9
+DATA_DIR = 'data'
+DATA_STUBS_DIR = '.data_stubs'
 
 
 class Bar(object):
@@ -254,7 +263,7 @@ def _extract_downloaded_archive(output_path):
         os.remove(output_path)
 
 
-def _process_dataset(dataset, output_dir, here):
+def _process_dataset(dataset, output_dir, here, use_test_data=False, force=False):
     '''Process each download spec in datasets.yml
 
     Typically each dataset list entry in the yml has
@@ -276,14 +285,14 @@ def _process_dataset(dataset, output_dir, here):
                 requires_download = True
                 break
 
-        if not requires_download:
+        if force is False and not requires_download:
             print('Skipping {0}'.format(dataset['title']))
             return
         url = dataset['url']
         title_fmt = dataset['title'] + ' {} of {}'
         if url.endswith('/'):
             urls = [url + f for f in dataset['files']]
-            output_paths = [os.path.join(here, 'data', fname)
+            output_paths = [os.path.join(here, DATA_DIR, fname)
                             for fname in dataset['files']]
 
             unpacked = ['.'.join(output_path.split('.')[:(-2 if output_path.endswith('gz') else -1)]) + '*'
@@ -297,11 +306,19 @@ def _process_dataset(dataset, output_dir, here):
         zipped = zip(urls, output_paths, unpacked)
         for idx, (url, output_path, unpack) in enumerate(zipped):
             running_title = title_fmt.format(idx + 1, len(urls))
-            if glob.glob(unpack) or os.path.exists(unpack.replace('*','')):
+            if force is False and (glob.glob(unpack) or os.path.exists(unpack.replace('*',''))):
                 # Skip a file if a similar one is downloaded:
                 # i.e. one that has same name but dif't extension
                 print('Skipping {0}'.format(running_title))
                 continue
+            test = os.path.join(output_dir, DATA_STUBS_DIR, unpack)
+            if use_test_data and os.path.exists(test):
+                target = os.path.join(output_dir, unpack)
+                print("Copying test data file '{0}' to '{1}'".format(test, target))
+                shutil.copyfile(test, target)
+                continue
+            elif use_test_data and not os.path.exists(test):
+                print("No test file found for: {}. Using regular file instead".format(test))
             _url_to_binary_write(url, output_path, running_title)
             _extract_downloaded_archive(output_path)
 
@@ -310,7 +327,7 @@ def _process_dataset(dataset, output_dir, here):
         print('this download script requires the requests module: conda install requests')
         sys.exit(1)
 
-def fetch_data(name,path,datasets="datasets.yml",require_datasets=True):
+def fetch_data(name,path,datasets="datasets.yml",require_datasets=True,use_test_data=False,force=False):
     '''Fetch sample datasets as defined by path/datasets if it exists or else module's own examples/datasets otherwise.
 
     Datasets are placed in path/data
@@ -323,15 +340,45 @@ def fetch_data(name,path,datasets="datasets.yml",require_datasets=True):
     if not os.path.exists(info_file) and require_datasets is False:
         print("No datasets to download")
         return
-        
-    print("Fetching data defined in %s and placing in %s"%(info_file,os.path.join(path,"data"))) # data is added later...
+
+    print("Fetching data defined in %s and placing in %s"%(info_file,os.path.join(path,DATA_DIR))) # data is added later...
 
     with open(info_file) as f:
         info = ordered_load(f.read())
         for topic, downloads in info.items():
             output_dir = os.path.join(path, topic)
             for d in downloads:
-                _process_dataset(d, output_dir, path)
+                _process_dataset(d, output_dir, path, use_test_data=use_test_data, force=force)
+
+def clean_data(name, path):
+    '''Remove up any data files that are copied from test files
+    '''
+    path = os.path.abspath(path)
+    if not os.path.exists(path):
+        path = _find_examples(name)
+
+    data_dir = os.path.join(path, DATA_DIR)
+    test_dir = os.path.join(data_dir, DATA_STUBS_DIR)
+    if not os.path.exists(test_dir) or len(os.listdir(test_dir)) == 0:
+        print("No test files found")
+        return
+
+    for f in os.listdir(test_dir):
+        data_file = os.path.join(data_dir, f)
+        if not os.path.isfile(data_file):
+            print("Test file was not copied to data:", f)
+            continue
+
+        test_file = os.path.join(test_dir, f)
+        if os.path.isfile(test_file):
+            data_s = os.path.getsize(data_file)
+            test_s = os.path.getsize(test_file)
+            if data_s == test_s:
+                print("Removing copied test file:", f)
+                os.remove(data_file)
+            else:
+                print("Size of test file {:.2e} did not match "
+                      "size of data file {:.2e}".format(test_s, data_s))
 
 
 # TODO: cmds=None defaults to 'all', basically, which is a bit confusing
@@ -344,7 +391,7 @@ def add_commands(parser,name,cmds=None,args=None):
 
     if cmds is None:
         # again a reg (duplicated in substitute_main)
-        cmds = ['examples','copy-examples','fetch-data']
+        cmds = ['examples','copy-examples','fetch-data','clean-data']
 
     # use dict/reg instead
     if 'copy-examples' in cmds:
@@ -356,31 +403,44 @@ def add_commands(parser,name,cmds=None,args=None):
 
     if 'fetch-data' in cmds:
         d_parser = parser.add_parser('fetch-data', help=inspect.getdoc(fetch_data))
-        d_parser.set_defaults(func=lambda args: fetch_data(name,args.path,args.datasets))
+        d_parser.set_defaults(func=lambda args: fetch_data(name,args.path,args.datasets,use_test_data=args.use_test_data,force=args.force))
         d_parser.add_argument('--path',type=str,help='where to put data',default='%s-examples'%name)
         d_parser.add_argument('--datasets',type=str,help='*name* of datasets file; must exist either in path specified by --path or in package/examples/',default='datasets.yml')
         d_parser.add_argument('-v', '--verbose', action='count', default=0)
+        d_parser.add_argument('--force',action='store_true', help='Force any existing data files to be replaced')
+        d_parser.add_argument('--use-test-data',action='store_true',
+                              help=("Use data's test files, if any, instead of fetching full data. "
+                                    "If test file not in '.data_stubs', fall back to fetching full data."))
 
     if 'examples' in cmds:
         egd_parser = parser.add_parser('examples', help=inspect.getdoc(examples))
-        egd_parser.set_defaults(func=lambda args: examples(name, args.path, args.verbose, args.force))
+        egd_parser.set_defaults(func=lambda args: examples(name, args.path, args.verbose, args.use_test_data, args.force))
         egd_parser.add_argument('--path',type=str,help='location to place examples and data',default='%s-examples'%name)
         egd_parser.add_argument('-v', '--verbose', action='count', default=0)
-        egd_parser.add_argument('--force', action='store_true', help='if PATH already exists, force overwrite existing examples if older than source examples')
+        egd_parser.add_argument('--force', action='store_true',
+                                help=('if PATH already exists, force overwrite existing examples if older '
+                                      'than source examples. ALSO force any existing data files to be replaced'))
+        egd_parser.add_argument('--use-test-data',action='store_true',
+                                help=("Use data's test files, if any, instead of fetching full data. "
+                                      "If test file not in '.data_stubs', fall back to fetching full data."))
 
+    if 'clean-data' in cmds:
+        cd_parser = parser.add_parser('clean-data', help=inspect.getdoc(clean_data))
+        cd_parser.set_defaults(func=lambda args: clean_data(name,args.path))
+        cd_parser.add_argument('--path',type=str,help='where to clean data',default='%s-examples'%name)
 
 def substitute_main(name,cmds=None,args=None):
     # can use if your module has no other commands
 
     if cmds is None:
         # again a reg
-        cmds = ['examples','copy-examples','fetch-data']
-    
+        cmds = ['examples','copy-examples','fetch-data', 'clean-data']
+
     mod = importlib.import_module(name)
     parser = argparse.ArgumentParser(description="%s commands"%name)
     parser.add_argument('--version', action='version', version='%(prog)s '+mod.__version__)
     subparsers = parser.add_subparsers(title='available commands')
     add_commands(subparsers,name,cmds,args)
     args = parser.parse_args()
-    args.func(args) if hasattr(args,'func') else parser.error("must supply command to run") 
-    
+    args.func(args) if hasattr(args,'func') else parser.error("must supply command to run")
+
---
+++ pyct/tests/test_cmd.py
@@ -0,0 +1,197 @@
+from pyct.cmd import fetch_data, clean_data, copy_examples, examples
+import pytest
+
+# Same as in pyct/examples/datasets.yml
+DATASETS_CONTENT = """
+data:
+  - url: this_should_never_be_used
+    title: 'Test Data'
+    files:
+      - test_data.csv
+"""
+
+# Same as in pyct/examples/data/.data_stubs/test_data.csv
+TEST_FILE_CONTENT = """
+name,score,rank
+Alice,100.5,1
+Bob,50.3,2
+Charlie,25,3
+"""
+
+REAL_FILE_CONTENT = """
+name,score,rank
+Alice,100.5,1
+Bob,50.3,2
+Charlie,25,3
+Dave,28,4
+Eve,25,3
+Frank,75,9
+"""
+
+FAKE_EXAMPLE_CONTENT = """
+import numpy as np
+
+a = np.arange(10)
+"""
+
+
+@pytest.fixture(scope='function')
+def tmp_project(tmp_path):
+    project = tmp_path / "test_project"
+    project.mkdir()
+    return project
+
+@pytest.fixture(scope='function')
+def tmp_project_with_examples(tmp_path):
+    project = tmp_path
+    examples = project / "examples"
+    examples.mkdir()
+    datasets = examples / "datasets.yml"
+    datasets.write_text(DATASETS_CONTENT)
+    (examples / "data").mkdir()
+    example = examples / "Test_Example_Notebook.ipynb"
+    example.write_text(FAKE_EXAMPLE_CONTENT)
+    return project
+
+@pytest.fixture(scope='function')
+def tmp_project_with_stubs(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    data_stubs = project / "examples" / "data" / ".data_stubs"
+    data_stubs.mkdir()
+    return project
+
+@pytest.fixture(scope='function')
+def tmp_project_with_test_file(tmp_project_with_stubs):
+    project = tmp_project_with_stubs
+    data_stub = project /  "examples" / "data" / ".data_stubs" / "test_data.csv"
+    data_stub.write_text(TEST_FILE_CONTENT)
+    return project
+
+
+def test_examples_with_use_test_data(tmp_project):
+    project = tmp_project
+    path = str(project / "examples")
+    examples(name="pyct", path=path, use_test_data=True)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+
+def test_examples_with_prexisting_content_in_target_raises_error(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    with pytest.raises(ValueError):
+        examples(name="pyct", path=path, use_test_data=True)
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").read_text() == FAKE_EXAMPLE_CONTENT
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() == REAL_FILE_CONTENT
+
+def test_examples_using_test_data_and_force_with_prexisting_content_in_target(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    examples(name="pyct", path=path, use_test_data=True, force=True)
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").read_text() != FAKE_EXAMPLE_CONTENT
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() != REAL_FILE_CONTENT
+
+def test_copy_examples(tmp_project):
+    project = tmp_project
+    path = str(project / "examples")
+    copy_examples(name="pyct", path=path)
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+
+def test_copy_examples_with_prexisting_content_in_target_raises_error(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    path = str(project / "examples")
+    with pytest.raises(ValueError):
+        copy_examples(name="pyct", path=path)
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").read_text() == FAKE_EXAMPLE_CONTENT
+
+def test_copy_examples_using_force_with_prexisting_content_in_target(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    path = str(project / "examples")
+    copy_examples(name="pyct", path=path, force=True)
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").is_file()
+    assert (project / "examples" / "Test_Example_Notebook.ipynb").read_text() != FAKE_EXAMPLE_CONTENT
+
+def test_fetch_data_using_test_data_with_no_file_in_data_copies_from_stubs(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    fetch_data(name=name, path=path, use_test_data=True)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() == TEST_FILE_CONTENT
+
+def test_fetch_data_using_test_data_with_file_in_data_skips(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    fetch_data(name=name, path=path, use_test_data=True)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() == REAL_FILE_CONTENT
+
+def test_fetch_data_using_test_data_and_force_with_file_in_data_over_writes(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    fetch_data(name=name, path=path, use_test_data=True, force=True)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() == TEST_FILE_CONTENT
+
+def test_clean_data_when_data_file_is_real_does_nothing(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    clean_data(name=name, path=path)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / "test_data.csv").read_text() == REAL_FILE_CONTENT
+
+def test_clean_data_when_data_file_is_from_stubs_removes_file_from_data(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(TEST_FILE_CONTENT)
+    clean_data(name=name, path=path)
+    assert not (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / ".data_stubs" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / ".data_stubs" / "test_data.csv").read_text() == TEST_FILE_CONTENT
+
+def test_clean_data_when_file_not_in_data_does_nothing(tmp_project_with_test_file):
+    project = tmp_project_with_test_file
+    name = 'pyct'
+    path = str(project / "examples")
+    clean_data(name=name, path=path)
+    assert not (project / "examples" / "data" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / ".data_stubs" / "test_data.csv").is_file()
+    assert (project / "examples" / "data" / ".data_stubs" / "test_data.csv").read_text() == TEST_FILE_CONTENT
+
+def test_clean_data_when_stubs_is_empty_does_nothing(tmp_project_with_stubs):
+    project = tmp_project_with_stubs
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    clean_data(name=name, path=path)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
+    assert not (project / "examples" / "data" / ".data_stubs" / "test_data.csv").is_file()
+
+def test_clean_data_when_no_stubs_dir_does_nothing(tmp_project_with_examples):
+    project = tmp_project_with_examples
+    name = 'pyct'
+    path = str(project / "examples")
+    data = project / "examples" / "data" / "test_data.csv"
+    data.write_text(REAL_FILE_CONTENT)
+    clean_data(name=name, path=path)
+    assert (project / "examples" / "data" / "test_data.csv").is_file()
