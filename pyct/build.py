5e0eb4616ca960111b87099a04ddb446a67a909b
Julia Signell <jsignell@gmail.com>
2019-02-18 13:24:40 -0500

    Changing inputs for examples and get_setup_version
    This would be a breaking change.

--- pyct/build.py
+++ pyct/build.py
@@ -1,27 +1,37 @@
 import os
 import shutil
 
-def examples(path, root, verbose=False, force=False):
+def examples(root, reponame, verbose=False, force=False):
     """
-    Copies the notebooks to the supplied path.
+    Copy example notebooks from original location to inside module
+    directory.
+    
+    Normally used in setup.py as follows: 
+    
+    >>> from pyct.build import examples
+    >>> examples(__file__, reponame)  # noqa
     """
     filepath = os.path.abspath(os.path.dirname(root))
-    example_dir = os.path.join(filepath, './examples')
-    if not os.path.exists(example_dir):
-        example_dir = os.path.join(filepath, '../examples')
-    if os.path.exists(path):
+    
+    old_example_dir = os.path.join(filepath, 'examples')
+    if not os.path.exists(old_example_dir):
+        old_example_dir = os.path.join(filepath, '..', 'examples')
+    if not os.path.exists(old_example_dir):
+        print('No example dir found in expected location')
+        return
+    
+    new_example_dir = os.path.join(filepath, reponame, 'examples')
+    if os.path.exists(new_example_dir):
         if not force:
-            print('%s directory already exists, either delete it or set the force flag' % path)
+            print('%s directory already exists, either delete it or set the force flag' % new_example_dir)
             return
-        shutil.rmtree(path)
+        shutil.rmtree(new_example_dir)
+    
     ignore = shutil.ignore_patterns('.ipynb_checkpoints', '*.pyc', '*~')
-    tree_root = os.path.abspath(example_dir)
-    if os.path.isdir(tree_root):
-        shutil.copytree(tree_root, path, ignore=ignore, symlinks=True)
-    else:
-        print('Cannot find %s' % tree_root)
+    shutil.copytree(old_example_dir, path, ignore=ignore, symlinks=True)
+    
 
-def get_setup_version(basepath, reponame):
+def get_setup_version(root, reponame):
     """
     Helper to get the current version from either git describe or the
     .version file (if available) - allows for param to not be available.
@@ -29,10 +39,11 @@ def get_setup_version(basepath, reponame):
     Normally used in setup.py as follows: 
     
     >>> from pyct.build import get_setup_version
-    >>> version = get_setup_version(os.path.dirname(__file__), reponame)  # noqa
+    >>> version = get_setup_version(__file__, reponame)  # noqa
     """
     import json
-    version_file_path = os.path.join(basepath, reponame, '.version')
+    filepath = os.path.abspath(os.path.dirname(root))
+    version_file_path = os.path.join(filepath, reponame, '.version')
     try:
         from param import version
     except:
@@ -42,3 +53,4 @@ def get_setup_version(basepath, reponame):
     else:
         print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
         return json.load(open(version_file_path, 'r'))['version_string']
+    
