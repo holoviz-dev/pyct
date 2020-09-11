dc98c5b5f3b92a123aeaf46ae83ef6b91186fef2
Chris B <ceball@users.sf.net>
2020-09-11 20:21:40 +0100

    Add missing api fn.

--- pyct/__init__.py
+++ pyct/__init__.py
@@ -2,6 +2,8 @@ import param
 
 NAME = "pyct"
 
+from .report import report  # noqa: api
+
 # version comes from git if available, otherwise from .version file
 __version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",
                                         reponame=NAME))
