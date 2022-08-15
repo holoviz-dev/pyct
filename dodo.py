001d936db48b3e5b1744e8132aecd0351fc2c0c7
maximlt <mliquet@anaconda.com>
2022-08-16 00:22:20 +0200

    add pip_on_conda

--- dodo.py
+++ dodo.py
@@ -1,1 +1,11 @@
 from pyctdev import *  # noqa: api
+
+
+def task_pip_on_conda():
+    """Experimental: provide pip build env via conda"""
+    return {'actions':[
+        # some ecosystem=pip build tools must be installed with conda when using conda...
+        'conda install -y pip twine wheel',
+        # ..and some are only available via conda-forge
+        'conda install -y -c conda-forge tox "virtualenv<=20.4.7"',
+    ]}
