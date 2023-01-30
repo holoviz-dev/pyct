b2733981a81855c07a5ce4c7c0622f8ee7039004
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2023-01-30 09:41:42 +0100

    General maintenance (#105)

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup_args = dict(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -61,7 +60,7 @@ setup_args = dict(
     },
     include_package_data=True,
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'param >=1.7.0',
     ],
